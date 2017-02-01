8ed1358148acb11d31a4cbe0a98556437df2b1d7
Pannous <info@pannous.com>
2017-02-01 19:39:59 +0100

    nix

--- lstm-tflearn.py
+++ lstm-tflearn.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import tflearn
-import speech_data
 import tensorflow as tf
+import tflearn
+
+import speech_data
 
 learning_rate = 0.0001
 training_iters = 300000  # steps
@@ -20,19 +21,16 @@ net = tflearn.fully_connected(net, classes, activation='softmax')
 net = tflearn.regression(net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
 model = tflearn.DNN(net, tensorboard_verbose=0)
 
-# Training
-
 ## add this "fix" for tensorflow version errors
 for x in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES): tf.add_to_collection(tf.GraphKeys.VARIABLES, x )
 
+# Training
 
 while --training_iters > 0:
-	X, Y = next(batch)
-	trainX, trainY = X, Y
-	testX, testY = X, Y  # overfit for now
-	model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
-          batch_size=batch_size)
+	trainX, trainY = next(batch)
+	testX, testY = next(batch)  # todo: proper ;)
+	model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True, batch_size=batch_size)
 
-_y=model.predict(next(batch)[0])
 model.save("tflearn.lstm.model")
+_y = model.predict(next(batch)[0])  # << add your own voice here
 print (_y)
