ee373a8ac267151c480823c48b47489bf86deaa3
Pannous <info@pannous.com>
2017-02-22 22:25:17 +0100

    qanda q_and_a sample data

---
+++ qanda/read_qanda.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import time
+import numpy
+import pyaudio
+from dateutil import parser
+import wave, struct
+audio = pyaudio.PyAudio()
+
+import csv
+
+# csvfile = open('qanda/qanda_2012_ep99_climate.csv')  # , newline='' is an invalid keyword
+# spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+# for row in spamreader:
+# 	try:
+# 		time, ida, kind, speaker, text = row
+# 		print(speaker)
+# 	# print(', '.join(row))
+# 	except:
+# 		pass
+import datetime
+
+path="qanda_2012_ep99_climate.srt"
+lines=open(path).readlines()
+i=0
+def milliseconds(offset):
+	time=parser.parse(offset).time()
+	return time.microsecond/1000 + 1000*(time.second + 60*(time.minute + 60* time.hour))
+
+
+waveFile = wave.open('qanda_2012_ep99_climate.wav', 'r')
+frameRate = waveFile.getframerate()
+out_stream = audio.open(format=pyaudio.paInt16, channels=waveFile.getnchannels(), rate=frameRate, output=True)
+out_stream.start_stream()
+
+length = waveFile.getnframes()
+print(frameRate)
+
+CHUNK = 1024
+def play(fro, to):
+	if fro>to or (to-fro)>3*1000:
+		print("segmented too long:")
+		print(fro)
+		print(to)
+		return
+	fro = fro* frameRate/1000
+	to = to * frameRate /1000
+	waveFile.setpos(fro)
+	dataraw = waveFile.readframes(2 * (to - fro)) # 2 ?? channels?
+	data0 = numpy.fromstring(dataraw, dtype='int16')
+	out_stream.write(data0)
+	# time.sleep(1)
+
+
+# arr=[ for waveData in waveDatas]
+
+
+while i<len(lines):
+	nr,offsets,word=lines[i:i+3]
+	print(word)
+	fro, to = offsets.split("-->")
+	fro = milliseconds(fro)
+	to = milliseconds(to)
+	play(fro,to)
+	# print(fro)
+	i += 4
+	# break
+
--- record.py
+++ record.py
@@ -19,7 +19,7 @@ plt.matshow([[1,0],[0,1]], fignum=1)
 plt.draw()
 
 if platform() == 'Darwin':  # How Mac OS X is identified by Python
-    system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+		system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
 
 i = 0
 width=256
@@ -51,75 +51,91 @@ image=numpy.array(bytearray(os.urandom(width*width)))
 image=image.reshape(width,width)
 
 def get_audio_input_stream():
-  INDEX = 0  # 1
-  # FORMAT = pyaudio.paInt8
-  FORMAT = pyaudio.paInt16
-  # FORMAT = pyaudio.paInt32
-  # FORMAT = pyaudio.paFloat32
-  CHANNELS = 1
-  # RATE = 22500
-  RATE = 48000 #* 2 = 96000Hz max on mac
-  INPUT_BLOCK_TIME = 0.05
-  # INPUT_BLOCK_TIME = 0.1
-  INPUT_FRAMES_PER_BLOCK = int(RATE * INPUT_BLOCK_TIME)
+	INDEX = 0  # 1
+	# FORMAT = pyaudio.paInt8
+	FORMAT = pyaudio.paInt16
+	# FORMAT = pyaudio.paInt32
+	# FORMAT = pyaudio.paFloat32
+	CHANNELS = 1
+	# RATE = 22500
+	RATE = 48000 #* 2 = 96000Hz max on mac
+	INPUT_BLOCK_TIME = 0.05
+	# INPUT_BLOCK_TIME = 0.1
+	INPUT_FRAMES_PER_BLOCK = int(RATE * INPUT_BLOCK_TIME)
 
-  stream = pyaudio.PyAudio().open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    frames_per_buffer=CHUNK,
-    input_device_index=INDEX)
-  return stream
+	stream = pyaudio.PyAudio().open(
+		format=FORMAT,
+		channels=CHANNELS,
+		rate=RATE,
+		input=True,
+		frames_per_buffer=CHUNK,
+		input_device_index=INDEX)
+	return stream
+
+
+	#hamming hanning bartlett 'blackman'
+hamming_window = np.hamming(length)  # minimize fourier frequency drain
 
 def record():
-  global i
-  global image
-  global winName
-  FILENAME = 'recording.wav'
-  stream = get_audio_input_stream()
-  # r = numpy.array()
-  hamming_window = np.hamming(length) # minimize fourier frequency drain
-  #hamming hanning bartlett 'blackman'
-  r = numpy.empty(length)
-  offset = 0
-  while True:
-    try:
-      dataraw = stream.read(CHUNK)
-    except IOError as e:
-      print(e) # [Errno -9981] Input overflowed  WHY?
-      stream=get_audio_input_stream()
-      continue
-    data0 = numpy.fromstring(dataraw, dtype='int16')
-    # data0 = numpy.fromstring(dataraw, dtype='int8')
-    if(i<20 and numpy.sum(np.abs(data0))<1000*width):
-      continue
-    r=numpy.append(r,data0)
-    while offset < r.size - length :
-      data = r[offset:offset+length]
-      data=data*hamming_window  # minimize fourier frequency drain
-      offset=offset + step
+	global i
+	global image
+	global winName
+	FILENAME = 'recording.wav'
+	stream = get_audio_input_stream()
+	r = numpy.empty(length)
+	offset = 0
+	while True:
+		try:
+			dataraw = stream.read(CHUNK)
+		except IOError as e:
+			print(e) # [Errno -9981] Input overflowed  WHY?
+			stream=get_audio_input_stream()
+			continue
+		data0 = numpy.fromstring(dataraw, dtype='int16')
+		# data0 = numpy.fromstring(dataraw, dtype='int8')
+		if(i<20 and numpy.sum(np.abs(data0))<1000*width):
+			continue
+		r=numpy.append(r,data0)
+		while offset < r.size - length :
+			data = r[offset:offset+length]
+			data=data*hamming_window  # minimize fourier frequency drain
+			offset=offset + step
 
-      data = numpy.fft.fft(data)#.abs()
-      data = numpy.absolute(data)
-      data = data[0:height]/256.0#.split(data,512)
-      data = numpy.log2(data*0.05+1.0)#//*50.0;
-      numpy.putmask(data, data > 255, 255)
+			data = numpy.fft.fft(data)#.abs()
+			data = numpy.absolute(data)
+			data = data[0:height]/256.0#.split(data,512)
+			data = numpy.log2(data*0.05+1.0)#//*50.0;
+			numpy.putmask(data, data > 255, 255)
 
-      image[i] = data
-      i = i+1
-      if(i==width):
-        print("i %d\r"%i)
-        i=0
-        # image=image.T
-        image=numpy.rot90(image)
-        plt.matshow(image, fignum=1)
-        plt.draw()
-        plt.pause(0.01)
-        # result=spec2word(image) #todo: reconnect
-        # subprocess.call(["say"," %s"%result])
-        # cv2.imshow(winName,image)
-        # if cv2.waitKey(10) == 27: BREAKS portAudio !!
+			image[i] = data
+			i = i+1
+			if(i==width):
+				print("i %d\r"%i)
+				i=0
+				# image=image.T
+				image=numpy.rot90(image)
+				plt.matshow(image, fignum=1)
+				plt.draw()
+				plt.pause(0.01)
+				# result=spec2word(image) #todo: reconnect
+				# subprocess.call(["say"," %s"%result])
+				# cv2.imshow(winName,image)
+				# if cv2.waitKey(10) == 27: BREAKS portAudio !!
 
 if __name__ == '__main__':
-  record()
+	record()
+
+
+def next_frame():
+	stream = get_audio_input_stream()
+
+	r = numpy.empty(length)
+	offset = 0
+	while True:
+		try:
+			dataraw = stream.read(CHUNK)
+			data0 = numpy.fromstring(dataraw, dtype='int16')
+			yield data0
+		except Exception as err:
+				print(err)
+				exit()
